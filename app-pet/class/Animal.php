df516181b4d40c366036eab47deb7e3a5627e43d
Guilherme Pereira Santanna <31224151+GuilhermeSantannape@users.noreply.github.com>
2019-12-01 22:15:33 -0300

    Add files via upload

--- app-pet/class/Animal.php
+++ app-pet/class/Animal.php
@@ -6,8 +6,8 @@
         public $dta_nasc;
         public $sexo;
 
-        function __construct($id, $desc_animal, $id_raca, $dta_nasc, $sexo){
-            $this->id = $id;
+        function __construct($id_animal, $desc_animal, $id_raca, $dta_nasc, $sexo){
+            $this->id_animal = $id_animal;
             $this->desc_animal = $desc_animal;
             $this->id_raca = $id_raca;            
             $this->dta_nasc = $dta_nasc;
