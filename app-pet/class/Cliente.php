e3b01db408378e06dc32c3d7c918b510dcf4cc9f
Guilherme Pereira Santanna <31224151+GuilhermeSantannape@users.noreply.github.com>
2019-12-04 05:35:24 -0300

    Add files via upload

--- app-pet/class/Cliente.php
+++ app-pet/class/Cliente.php
@@ -1,16 +1,25 @@
 <?php
     class Cliente {
-        public $id_pessoa;
+        public $id_cliente;
         public $nome;
         public $cpf;
         public $sexo;
+        public $email;
+        public $endereco;
+        public $numero;
+        public $complemente;
+        
        
 
-        function __construct($id_pessoa, $nome, $cpf, $sexo){
-            $this->id_pessoa = $id_pessoa;
+        function __construct($id_cliente, $nome, $cpf, $sexo,$email,$endereco,$numero,$complemente){
+            $this->id_cliente = $id_cliente;
             $this->nome = $nome;
-            $this->cpf = $cpf;            
+            $this->cpf = $cpf;
             $this->sexo = $sexo;
+            $this->email = $email;            
+            $this->endereco = $endereco;
+            $this->numero = $numero;
+            $this->complemente = $complemente;
           
         }
     }
