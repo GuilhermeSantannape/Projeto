570eb54c58850dc8087015e44099b4fb5cc989f5
Guilherme Pereira Santanna <31224151+GuilhermeSantannape@users.noreply.github.com>
2019-12-12 20:05:48 -0300

    alterações do banco feito em aula

--- wsprodutos.sql
+++ wsprodutos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 04-Dez-2019 às 09:25
+-- Tempo de geração: 13-Dez-2019 às 00:06
 -- Versão do servidor: 10.4.6-MariaDB
 -- versão do PHP: 7.3.9
 
@@ -33,7 +33,7 @@ CREATE TABLE `animais` (
   `desc_animal` varchar(50) NOT NULL,
   `id_raca` int(11) NOT NULL,
   `dta_nasc` date NOT NULL,
-  `sexo` varchar(1) DEFAULT NULL
+  `sexo` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -53,7 +53,36 @@ INSERT INTO `animais` (`id_animal`, `desc_animal`, `id_raca`, `dta_nasc`, `sexo`
 (10, 'chuck', 2, '2019-12-01', 'M'),
 (11, 'budemina', 2, '2019-12-01', 'M'),
 (12, 'GUILHERME PEREIRA SANTANNA', 1, '1994-05-25', 'a'),
-(13, 'GUILHERME ', 0, '2019-12-04', 'a');
+(13, 'GUILHERME ', 0, '2019-12-04', 'a'),
+(19, '', -1, '0000-00-00', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `cpf` int(11) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `endereco` varchar(100) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `complemente` varchar(50) DEFAULT NULL,
+  `senha` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id_cliente`, `nome`, `cpf`, `sexo`, `email`, `endereco`, `numero`, `complemente`, `senha`) VALUES
+(4, 'guilherme', 12345678, '1', 'guilhermesantanna.mail@gmail.com', 'Rua Domingos ', 215, '123', 1310),
+(5, 'GUILHERME PEREIRA SANTANNA', 0, '0', 'guilhermesantanna.mail@gmail.com', 'Rua Domingos Crescêncio, Ap 201', 2, 'asda', NULL),
+(6, 'GUILHERME PEREIRA SANTANNA', 12345678, '-2', 'guilhermesantanna.mail@gmail.com', 'Rua Domingos Crescêncio, Ap 201', 0, '', NULL),
+(7, '', 12345678, '0', '', '', 0, '', NULL);
 
 -- --------------------------------------------------------
 
@@ -100,17 +129,7 @@ CREATE TABLE `produtos` (
 --
 
 INSERT INTO `produtos` (`id`, `nome`, `imagem`, `descricao`, `uso`) VALUES
-(1, 'Modelo Ragon WZasdd', 'img/motor-1.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'veicular'),
-(2, 'Modelo Ragon TY', 'img/motor-2.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'veicular'),
-(3, 'Modelo Delta', 'img/motor-3.jpg', 'Cabeçotes individuais com 6 válvulas por cilindro', 'veicular, maritimo'),
-(4, 'Modelo Dyna', 'img/motor-1.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'agricola'),
-(5, 'Modelo Tork G1', 'img/motor-2.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'agricola, industrial'),
-(6, 'Modelo Tork JA', 'img/motor-3.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'industrial'),
-(7, 'Modelo Combo Alfa', 'img/motor-1.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'maritimo'),
-(8, 'Modelo Combo Beta', 'img/motor-2.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'maritimo'),
-(9, 'GUILHERME PEREIRA SANTANNA', 'asdas', 'asd', 'asd'),
-(11, 'GUILHERME PEREIRA SANTANNA', 's', 's', 's'),
-(12, 'Modelo Ragon WZ', 'img/motor-1.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'veicular');
+(4, 'Modelo Dyna', 'img/recao.jpg', 'Cabeçotes individuais com 4 válvulas por cilindro', 'agricola');
 
 -- --------------------------------------------------------
 
@@ -172,6 +191,24 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`id`, `nome`, `login`, `senha`) VALUES
 (1, 'guilherme', 'guilherme', '131092');
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios_token`
+--
+
+CREATE TABLE `usuarios_token` (
+  `id` int(11) NOT NULL,
+  `token` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuarios_token`
+--
+
+INSERT INTO `usuarios_token` (`id`, `token`) VALUES
+(1, 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c3VhcmlvX2lkIjoxLCJ1c3VhcmlvX25vbWUiOiJndWlsaGVybWUifQ.lZex4Uy0j3g9wbLihUIyPzesy4He5nXT8bD1ENJEp1E');
+
 --
 -- Índices para tabelas despejadas
 --
@@ -181,6 +218,12 @@ INSERT INTO `usuarios` (`id`, `nome`, `login`, `senha`) VALUES
 --
 ALTER TABLE `animais`
   ADD PRIMARY KEY (`id_animal`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
 
 --
 -- Índices para tabela `produtos`
@@ -208,6 +251,13 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Índices para tabela `usuarios_token`
+--
+ALTER TABLE `usuarios_token`
+  ADD PRIMARY KEY (`token`),
+  ADD KEY `fk_usuarios` (`id`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -215,13 +265,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `animais`
 --
 ALTER TABLE `animais`
-  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_animal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT de tabela `raca`
@@ -234,6 +290,16 @@ ALTER TABLE `raca`
 --
 ALTER TABLE `tipo_animal`
   MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `usuarios_token`
+--
+ALTER TABLE `usuarios_token`
+  ADD CONSTRAINT `fk_usuarios` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
